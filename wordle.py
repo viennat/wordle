a0e077e3e723cb3ec3bf250d3891704bd8c0aaa2
viennat <46495482+viennat@users.noreply.github.com>
2022-01-12 13:15:58 -0800

    Update wordle.py

--- wordle.py
+++ wordle.py
@@ -9,30 +9,45 @@ import random
 word_list = open('/Users/viennathomas/Desktop/Not School/viennawordle/wordlist.txt').readlines()
 mystery_number = random.randrange(5755)
 mystery_word = word_list[mystery_number]
+#mystery_word = 'quark'
 counter = 6
-while counter >0: 
+starting_blanks= ['_','_','_','_','_']
+
+while counter > 0 : 
+    print ("\n The word so far is: \n")
+    print(*starting_blanks, sep= ' ')
     guess = str(input("You have " +str(counter)+ " guesses left. Please Enter Your Word Guess: "))
+    number_correct_letters = 0
     if len(guess)!=5: 
         print("Hey that doesn't have five letters bro...")
         break
+         # want to just go back to the guess without changing counter or exiting out of the run
+#    if guess not in word_list:
+#        print("Come on... that's not a word... Try again")
+#        break 
+        #dk how to get this to work
     if guess == mystery_word:
-        print(" Congrats! You have guessed the word correctly. It took you "+ str(6-counter)+" tries.")
+                print(" Congrats! You have guessed the word correctly. It took you "+ str(7-counter)+" tries.")
+                break
     else:
         for i in range(5):
             if guess[i] in mystery_word:
+               
                 if guess[i] == mystery_word[i]:
+                    starting_blanks[i] = guess[i]
+                    number_correct_letters+=1
                     print(guess[i]+" is in the correct position and is in the mystery word" )
                     i+=1
+                    if number_correct_letters == 5:
+                        print(" Congrats! You have guessed the word correctly. It took you "+ str(7-counter)+" tries.")
+                        break 
                 else: 
                     print(guess[i]+" is not in the correct position but is in the mystery word")
                     i+=1
             else:
                 print(guess[i]+" is not in the mystery word")
-                i+=1
-     #fix win condition           
-            
+                i+=1          
     counter-=1
-    if counter == 0 :
-        print("You have run out of guesses. The word is " + mystery_word)
-        counter = 6
-        break
+if counter == 0 :
+    print("You have run out of guesses. The word is " + mystery_word)
+    counter = 6
